caa86659560ae20101d99877d10bc332fd840c43
watanabe nami <210392v@ugs.kochi-tech.ac.jp>
2020-09-20 16:13:18 +0900

    完成

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sum(){
+        number += 1
+        font()
+    }
+    
+    @IBAction func sub(){
+           number -= 1
+           font()
+       }
+    
+    @IBAction func mul(){
+           number *= 2
+           font()
+       }
+    
+    @IBAction func div(){
+           number /= 2
+           font()
+       }
+    
+    @IBAction func clear(){
+        number = 0
+        font()
+    }
+    
+    func font(){
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+        label.text = String(number)
+    }
 }
 
